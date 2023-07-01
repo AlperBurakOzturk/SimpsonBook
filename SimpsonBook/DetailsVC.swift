ec75ab2630b3ffa7ec05dda7989c12b83c8a64e0
Alper Burak Öztürk <alperrburak@gmail.com>
2023-07-01 17:27:48 +0300

    Initial Commit

---
+++ SimpsonBook/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  SimpsonBook
+//
+//  Created by Alper Burak Öztürk on 19.04.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
+    }
+    
+
+  
+
+}
